656590de9e8501fa5b2d004f29fb4670ae0cc1a5
aeinspanier <72575207+aeinspanier@users.noreply.github.com>
2025-09-27 09:48:07 -0500

    adding reporting layer

--- models/intermediate/chicago_establishment_inspections_inter.sql
+++ models/intermediate/chicago_establishment_inspections_inter.sql
@@ -1,4 +1,8 @@
-{{ config(materialized='table') }}
+{{ config(
+            materialized='table',
+            unique_key=['inspection_id'],
+            tags=['intermediate', 'silver', 'chicago']
+) }}
 
 
 with inspections as (
---
+++ models/reporting/date/inspection_metrics_date.sql
@@ -0,0 +1,63 @@
+{{ config(
+            materialized='table',
+            unique_key=['inspection_id'],
+            tags=['reporting', 'inspection_metrics', 'date']
+) }}
+
+with source as (
+  select
+    city_tag,
+    business_id,
+    inspection_id,
+    business_name,
+    inspection_status,
+    business_address,
+    inspection_date,
+    inspection_type,
+    inspection_last_updated
+  from {{ ref('health_inspections_inter') }}
+)
+
+, aggregated as (
+ 
+  select
+    inspection_date as date,
+    city_tag as city,
+    count(distinct inspection_id) as total_inspections,
+    count(case when inspection_status = 'PASS' then 1 end) as passing_inspections,
+    count(case when inspection_status = 'FAIL' then 1 end) as failing_inspections,
+    count(case when inspection_status = 'UNKNOWN' then 1 end) as inconclusive_inspections
+  from source
+  group by inspection_date, city_tag
+)
+
+, reporting as (
+  select
+    date,
+    city,
+    total_inspections,
+    passing_inspections,
+    failing_inspections,
+    inconclusive_inspections,
+    (passing_inspections / total_inspections) * 100.0 as pass_rate,
+    (failing_inspections / total_inspections) * 100.0 as fail_rate,
+    (inconclusive_inspections / total_inspections) * 100.0 as inconclusive_rate
+  from aggregated
+)
+
+, final as (
+  select
+    date,
+    city,
+    total_inspections,
+    passing_inspections,
+    failing_inspections,
+    inconclusive_inspections,
+    pass_rate,
+    fail_rate,
+    inconclusive_rate
+  from reporting
+)
+
+select * from final
+
